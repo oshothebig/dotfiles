7604145c87da6392c316ce1d9891881ad744a333
Sho SHIMIZU <sho.shimizu@gmail.com>
2024-08-12 16:45:56 +0900

    Add config for Wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+-- Font
+config.font_size = 13.0
+
+-- Scroll
+config.scrollback_lines = 100000
+
+-- Tab
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Color scheme
+config.color_scheme = 'nord'
+
+-- and finally, return the configuration to wezterm
+return config
